487ffedfcac598457a0c40d3399019a91d4543c8
Nahuel Luca <nahuelluca20@gmail.com>
2024-02-06 16:21:42 -0300

    Update page and footer components

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,7 +1,9 @@
 export default function Footer() {
   return (
-    <div className="fixed bottom-0  py-5 w-full">
-      <footer className="mx-auto max-w-[1080px] bg-red-200 ">footer</footer>
+    <div className="fixed bottom-0 w-full">
+      <footer className="mx-auto py-5 max-w-[1080px] bg-red-200 ">
+        footer
+      </footer>
     </div>
   );
 }
