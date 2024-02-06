d9935f29a80a6aab11fff6efe28fda111214cf3e
Nahuel Luca <nahuelluca20@gmail.com>
2024-02-06 17:10:04 -0300

    Refactor home page to support internationalization

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -1,3 +1,28 @@
-export default function Home() {
-  return <main className="">dsa</main>;
+import { Button } from "@/components/ui/button";
+import { Locale } from "@/i18n-config";
+import { getDictionary } from "@/utils/dictionaries";
+import Link from "next/link";
+
+export default async function Home({ params }: { params: { lang: Locale } }) {
+  const dictionary = await getDictionary("home", params.lang);
+  return (
+    <main className="">
+      <div className="space-y-2">
+        <h1 className="font-bold text-2xl">{dictionary.title}</h1>
+        <h4 className="font-semibold text-xl text-muted-foreground">
+          {dictionary.subtitle}
+        </h4>
+      </div>
+      <p className="mt-5">{dictionary.content}</p>
+
+      <div className="mt-10">
+        <h2 className="text-lg font-medium">{dictionary.swithLanguage}</h2>
+        <Button asChild>
+          <Link href="/without-Internationalization-link">
+            {dictionary.link}
+          </Link>
+        </Button>
+      </div>
+    </main>
+  );
 }
---
+++ utils/dictionaries.ts
@@ -0,0 +1,36 @@
+import "server-only";
+import { Locale } from "@/i18n-config";
+
+interface DictionaryPaths {
+  [section: string]: {
+    [locale in Locale]: () => Promise<any>;
+  };
+}
+
+const dictionaryPaths: DictionaryPaths = {
+  home: {
+    en: () =>
+      import("../dictionaries/en/home.json").then((module) => module.default),
+    es: () =>
+      import("../dictionaries/es/home.json").then((module) => module.default),
+  },
+};
+
+export const getDictionary = async (
+  section: string,
+  locale: Locale
+): Promise<any> => {
+  const sectionDictionary = dictionaryPaths[section];
+
+  if (!sectionDictionary) {
+    throw new Error(`Invalid dictionary section: ${section}`);
+  }
+
+  const getLocaleDictionary = sectionDictionary[locale];
+
+  if (!getLocaleDictionary) {
+    throw new Error(`Invalid locale for section ${section}: ${locale}`);
+  }
+
+  return getLocaleDictionary();
+};
