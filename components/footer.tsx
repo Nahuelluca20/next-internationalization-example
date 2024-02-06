b0a0d3e45d645043421677e252c6c5a8f11e6af1
Nahuel Luca <nahuelluca20@gmail.com>
2024-02-06 15:47:44 -0300

    Add Footer component and update layout styles

---
+++ components/footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <div className="fixed bottom-0  py-5 w-full">
+      <footer className="mx-auto max-w-[1080px] bg-red-200 ">footer</footer>
+    </div>
+  );
+}
